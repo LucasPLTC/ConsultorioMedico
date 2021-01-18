72f10f48f242b6982f1273540983b70af7e9fcbe
Lucas Carvalho <lucas.carvalho.pltc@gmail.com>
2021-01-17 22:52:23 -0300

    Commit inicial

---
+++ src/main/java/com/oficina/Model/Pessoa.java
@@ -0,0 +1,14 @@
+package com.oficina.Model;
+
+import java.util.Date;
+
+public abstract class Pessoa {
+    Integer id;
+    String nome;
+    String email;
+    String cpf;
+    String telefone;
+    Date dtnasc;
+
+
+}
